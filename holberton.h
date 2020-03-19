2fee6ba6929388452e8545d76fce4cce0471d85f
saidskander <askander199h@outlook.com>
2020-03-19 04:09:55 +0100

    header

--- holberton.h
+++ holberton.h
@@ -6,13 +6,15 @@ int _putchar(char c);
 
 int _printf(const char *format, ...);
 
-int put_c(va_list argf);
+int print_c(va_list argf);
 
-int put_s(va_list argf);
+int print_s(va_list argf);
 
-int put_i(va_list argf);
+int print_h(va_list argf);
 
-int put_n(va_list argf);
+int print_n(va_list argf);
+
+int print_ex (va_list argf);
 
 /**
  * struct forms - forms
@@ -20,11 +22,11 @@ int put_n(va_list argf);
  * @fn: function
  * Description: empty
  */
-typedef struct forms
+typedef struct forms_t
 {
 char x;
 int (*fn)(va_list argf);
 }
-forms;
-int struu(const char *format, int y, va_list argf);
+forms_t;
+int st(const char *format, int y, va_list argf);
 #endif
