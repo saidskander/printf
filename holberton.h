5dc7b9913fead630211e07ae978c93b403c43f11
saidskander <askander199h@outlook.com>
2020-03-20 21:04:13 +0100

    honor mariem

--- holberton.h
+++ holberton.h
@@ -16,17 +16,4 @@ int print_n(va_list argf);
 
 int print_ex (va_list argf);
 
-/**
- * struct forms - forms
- * @x: character type
- * @fn: function
- * Description: empty
- */
-typedef struct forms_t
-{
-char x;
-int (*fn)(va_list argf);
-}
-forms_t;
-int st(const char *format, int y, va_list argf);
 #endif
---
+++ printf.c
@@ -0,0 +1,43 @@
+#include <stdarg.h>
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _printf - _printf
+ * @format : char
+ * Return: something 
+ */
+int _printf(const char *format, ...)
+{
+int x = 0, count = 0;
+char *ks;
+int j;
+va_list forms;
+char k;
+va_start(forms, format);
+for (x = 0; format[x] != '\0'; x++)
+{
+if ((format[x] == '%') && (format[x + 1] == 'c'))
+{
+k =(char)va_arg(forms, int);
+_putchar(k);
+count++;
+x++;
+}
+else if ((format[x] == '%') && (format[x + 1] == 's'))
+{
+ks =va_arg(forms, char*);
+for (j = 0; ks[j] != '\0'; j++)
+_putchar(ks[j]);
+count = count + j;
+x++;
+}
+else
+{
+_putchar(format[x]);
+count++;
+}
+}
+va_end(forms);
+return (count);
+}
